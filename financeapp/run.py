4643500107f80dfe48212ade18fced7c1f9ed33a
KevLoughrey <kevloughrey@gmail.com>
2025-06-25 15:20:28 +0100

    chore(app): add dotenv and update requirements

--- financeapp/run.py
+++ financeapp/run.py
@@ -1,7 +1,13 @@
+import os
 from flask import Flask
 from home import home_bp
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 app.register_blueprint(home_bp, url_prefix='')
 
